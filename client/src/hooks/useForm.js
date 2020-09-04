6cdccdd18836747481866fd83b8cbb27ff9d845c
AJ Gambino <aj.gambino7@gmail.com>
2020-09-04 11:46:48 -0400

    plantList and useForm finished

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,29 @@
 // write your custom hook here to control your checkout form
+import {useState, useEffect} from 'react'
+
+
+export const useForm = (initialData) => {
+
+  const [values, setValues] = useState(initialData)
+
+  /*   const clearForm = e => {
+        e.preventDefault();
+        setValues(initialValues);
+      }; */
+
+    const handleChanges = evt => {
+        setValues({
+            ...values,
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+
+
+
+    return (
+        [handleChanges, values, /* clearForm */ showSuccessMessage, setShowSuccessMessage]
+    )
+}
+
